e1dee84f8ce3d4865e1fc1247529080610d35b79
Tatiana Shuvainikova <shuvainikova.tp@phystech.edu>
2021-12-16 15:44:33 +0300

    переполнение мантисы - теперь работает

---
+++ 2-new.cpp
@@ -0,0 +1,49 @@
+#include <iostream> //переполнение мантиссы
+#include <cmath>
+using namespace std;
+
+void unint2bin(unsigned int x){
+
+	int a[35];
+	for (int i=31; i>=0; i--){
+		unsigned int y = x;
+		x>>=1;
+		x<<=1;
+		if (x==y)
+			a[i]=0;
+		else
+			a[i]=1;
+		x>>=1;
+
+	}
+	for (int i=0; i<32;){
+	    if (a[i]==0){
+	        i = i+1;
+	    }
+	    else{
+	        for(int j = i; j<32; j++){
+	            cout<<a[j];
+	        }
+	        break;
+	    }
+	}
+	
+}
+
+int main(){
+	unsigned long long int n = 2;
+	while (true){
+		n *= n;
+		cout<<(n)<<endl;
+		unint2bin (n);
+		cin.get();
+	}
+
+	
+
+
+
+
+
+	return 0;
+}
